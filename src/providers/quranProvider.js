138c9cef3711328b35b28bd8a7d0bef77bc0415a
mostafaaamen <88153348+mostafaaamen@users.noreply.github.com>
2026-07-12 23:50:16 +0300

    Update quranProvider.js

--- src/providers/quranProvider.js
+++ src/providers/quranProvider.js
@@ -1,11 +1,17 @@
 import getRandomAyahs from '../aya.js';
 import { PATHS } from '../config/constants.js';
-export function getQuranPayload() {
+import { getFormattedAyas } from '../../src/utils/tafther.js';
+
+// 1. أضفنا async للدالة
+export async function getQuranPayload() {
   const data = getRandomAyahs();
+  
+  // 2. أضفنا await للانتظار لحين جلب البيانات من الـ API
+  const result = await getFormattedAyas(data.number, data.startAyahId, data.endAyahId);
   return {
-    text: data.text,
-    metaText: `سورة ${data.surah} - الآية (${data.ayah}) عدد الآيات ${data.totalAyahs}`,
-    caption: `سورة ${data.surah} - الآيات (${data.ayah}) من ${data.totalAyahs} آية\n\n#القرآن_الكريم`,
-    bgImagePath: PATHS.defaultImage // صورة خاصة بالقرآن
+    text: data.text, // نضع الآيات المنسقة مع التفسير لترسم على الصورة
+    metaText: `سورة ${data.surah} - الآية (${data.startAyahId} - ${data.endAyahId}) عدد الآيات ${data.totalAyahs}`,
+    caption: `${result}\n\n#القران_الكريم\n\n #التفسير_الميسر`,
+    bgImagePath: PATHS.defaultImage
   };
 }
